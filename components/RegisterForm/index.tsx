6bdb8df8aab78ba5ababfb787fba570b79412139
Germs31 <german90nunez@gmail.com>
2024-11-30 23:32:50 -0800

    register page and form

--- components/RegisterForm/index.tsx
+++ components/RegisterForm/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Link from "next/link";
+import Link from 'next/link'
 
-const LoginForm = () => {
+
+const RegisterForm = () => {
   return (
     <div className='grid place-items-center h-screen'>
         <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-            <h1 className='text-xl font-bold my-4'>Enter Credentials</h1>
+            <h1 className='text-xl font-bold my-4'>Register</h1>
 
             <form className="flex flex-col gap-3">
+                <input type="text" placeholder="full name"/>
                 <input type="text" placeholder="email"/>
                 <input type="password" placeholder="password"/>
                 <button className='bg-green-600 text-white font-bold cursor px-6 py-2'>Login</button>
@@ -16,8 +18,8 @@ const LoginForm = () => {
                     error msg
                 </div>
 
-                <Link className="text-sm mt-3 text-right" href={"/register"}>
-                    Don't have an account? <span className="underline">Register</span>
+                <Link className="text-sm mt-3 text-right" href={"/"}>
+                    have an account? <span className="underline">Login</span>
                 </Link>
             </form>
         </div>
@@ -25,4 +27,4 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm
+export default RegisterForm
